22dd8c4f718a525431ab9a7bfe4fbd4b6989dd9d
guchi3f4 <guchi3f4@gmail.com>
2021-11-03 12:13:27 +0000

    フォローカウント非同期

--- app/controllers/user_relations_controller.rb
+++ app/controllers/user_relations_controller.rb
@@ -2,12 +2,20 @@ class UserRelationsController < ApplicationController
   def create
     @user = User.find(params[:user_id])
     current_user.active_relations.create(followed_id: @user.id)
-    render 'users/user_follow'
+    if request.referer.include?(user_path(@user))
+      render 'users/user_follow'
+    else
+      render 'users/user_info'
+    end
   end
 
   def destroy
     @user = User.find(params[:user_id])
     current_user.active_relations.find_by(followed_id: @user.id).destroy
-    render 'users/user_follow'
+    if request.referer.include?(user_path(@user))
+      render 'users/user_follow'
+    else
+      render 'users/user_info'
+    end
   end
 end
