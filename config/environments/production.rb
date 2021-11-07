1068987bc1bd5e8cde2ff27bf78df8250b4a9ff9
guchi3f4 <guchi3f4@gmail.com>
2021-11-07 11:07:32 +0000

    ドメイン

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  host = '52.196.236.87/'
+  host = 'codience.work/'
   config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 
   config.action_mailer.delivery_method = :smtp
